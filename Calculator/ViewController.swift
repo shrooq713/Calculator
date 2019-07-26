80e0b9344c83ec59235a4ddb24b55eaae54bc5f1
shrooqsaleh <shshrooq12@gmail.com>
2019-07-26 09:45:04 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Shrooq Saleh on 26.07.19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
